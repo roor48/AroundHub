d64d5e1d7addf5df4e448025ffc87ee02fb58825
roor48 <kmgoio@naver.com>
2025-07-08 18:53:14 +0900

    Init

---
+++ src/test/java/study/min/aroundhub/AroundHubApplicationTests.java
@@ -0,0 +1,13 @@
+package study.min.aroundhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
