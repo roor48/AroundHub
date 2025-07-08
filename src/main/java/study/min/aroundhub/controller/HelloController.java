05c98a752204bc48493179620240e3dc9bace285
roor48 <kmgoio@naver.com>
2025-07-09 00:52:20 +0900

    Springdoc OpenAPI사용

--- src/main/java/study/min/aroundhub/controller/HelloController.java
+++ src/main/java/study/min/aroundhub/controller/HelloController.java
@@ -4,5 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
